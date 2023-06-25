8656b5c2ac6fdf310520911e1114a51613ad399f
Azamat <hasanjonovazamat@gmail.com>
2023-06-25 12:52:02 +0500

    Project Structure

---
+++ src/main/java/uz/pdp/hospitalqueuemanagement/entity/dr/DrEntityType.java
@@ -0,0 +1,8 @@
+package uz.pdp.hospitalqueuemanagement.entity.dr;
+
+public enum DrEntityType {
+    Surgeon,General_practitioner,Neurologist,Pediatrician,Dermatologist,Psychiatrist,Cardiologist,
+    Radiologist,Oncologist,Anesthesiologist,Urologist,Orthopaedist,Pathologist,Family_medicine,
+    Internal_medicine,Ophthalmology,Rheumatologist,Obstetrics_gynaecology,Gastroenterologist,
+    Endocrinologist,Neurology,Ophthalmologist,Dermatology,Psychiatry
+}
---
+++ src/main/java/uz/pdp/hospitalqueuemanagement/entity/queue/QueueEntity.java
@@ -0,0 +1,23 @@
+package uz.pdp.hospitalqueuemanagement.entity.queue;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
+import lombok.*;
+import uz.pdp.hospitalqueuemanagement.entity.BaseEntity;
+import uz.pdp.hospitalqueuemanagement.entity.dr.DrEntity;
+import uz.pdp.hospitalqueuemanagement.entity.user.UserEntity;
+
+@Entity(name = "queues")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+public class QueueEntity extends BaseEntity {
+    @OneToOne
+    private UserEntity user;
+    private Long queueNumber;
+    @ManyToOne
+    private DrEntity doctor;
+}
