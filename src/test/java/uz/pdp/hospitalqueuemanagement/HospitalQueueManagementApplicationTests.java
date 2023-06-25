2cd32245957c10823535e102ddb7a73217fb4b91
Azamat <hasanjonovazamat@gmail.com>
2023-06-25 11:18:49 +0500

    Initial commit

---
+++ src/test/java/uz/pdp/hospitalqueuemanagement/HospitalQueueManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.hospitalqueuemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalQueueManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
