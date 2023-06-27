bec75aa8c255b467272bd6a735a67ef106166ea4
Azamat <hasanjonovazamat@gmail.com>
2023-06-27 15:20:22 +0500

    queue service v1

--- src/main/java/uz/pdp/hospitalqueuemanagement/repository/QueueRepository.java
+++ src/main/java/uz/pdp/hospitalqueuemanagement/repository/QueueRepository.java
@@ -1,11 +1,15 @@
 package uz.pdp.hospitalqueuemanagement.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import uz.pdp.hospitalqueuemanagement.entity.dr.DrEntity;
+import uz.pdp.hospitalqueuemanagement.entity.queue.QueueEntity;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
-public interface QueueRepository extends JpaRepository<DrEntity, UUID> {
+public interface QueueRepository extends JpaRepository<QueueEntity, UUID> {
+    @Query("select max(q.queueNumber) from queues q")
+    Optional<Long> lastQueue();
 }
